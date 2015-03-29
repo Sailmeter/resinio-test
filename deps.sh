cca565020be616a754757e5c94e3a39001dd5b7e
Mark Allen <mark@markcallen.com>
2015-03-29 14:54:35 -0700

    Added build-essential

--- deps.sh
+++ deps.sh
@@ -9,8 +9,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Do something under Linux platform
   echo "Linux"
   apt-get -q update
-  apt-get install -y cowsay
-  /usr/games/cowsay "Install dependencies using a script like this!";
+  apt-get install -y build-essential
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
   # Do something under Windows NT platform
   echo "Windows"
